d967b13191a0b593bef8c38bc31ce9db64c65cb8
marhabat <marhi.ashimovna@gmail.com>
2023-01-05 13:27:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,284 @@
+// 1 Number type
+//123456789
+
+//2 String
+//'hello'
+
+//3 Boolean
+// true false
+
+//4 Underfire
+
+// let a="hello"
+
+
+// const constata
+
+//var c="hello" устаревший уже не пользуется
+
+//console.log(typeof NaN
+//console.log(6==='6') сравнение все
+
+//let a = 5
+//let b = 7
+//console.log(5+7)
+
+//let a = 6
+//if ("a<10"){
+  //  console.log(a<10)
+
+//}
+
+
+
+
+//let greeting = 'Hello'
+
+//console.log(greeting)
+
+//let a = 8
+//let b = 6
+
+//console.log(a%b)
+
+//6/2===0
+
+//let a= 6
+
+//if( a%2 === 0){
+//    console.log("even")
+//} else{
+  //  console.log("odd")
+//}
+
+//alert("Hello")
+
+
+//let a = prompt("type any number")
+//if(a%2===0){
+  //  console.log("even")
+//}else{
+  //  console.log("odd")
+//}
+
+//if(a<10){
+  //  console.log("a<10")
+//}else if ("a>10")
+
+//let a = prompt("type any number")
+//if(a<10){
+  //  console.log(a*2)
+
+//}else if (a>10){
+  //  console.log(a/2)
+//}else{
+  //  console.log(a)
+//}
+
+//let a = -22;
+//console.log(a * -1)
+//if (a<0){
+  //  console.log(a*(-1))
+//}
+
+//let a = prompt ('type any number')
+//if (a=10){
+  //console.log('Верно')
+//}
+//else{
+//  console.log('Не верно')
+//}
+
+
+// 1. Вам дана переменная a. Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Неверно'.
+// 2. Вам даны две переменные a=50, b=100. Составьте условие, по которому в консоль будет выводиться 'a больше b', 'a меньше b'.
+// 3. Вам дана переменная a=-2. Составьте условие, по которому в консоль будет выводиться 'positive' - если число больше 0, 'равно' если число равно 0, "negative" если число меньше 0.
+// 4. Вам дана переменная number=45. Составьте условие, по которому будет выводиться в консоль "Четное число", "Нечетное число".
+// 5. Вам даны две переменные a=10, b=2. Составьте условие, по которому, остаток деления a на b, будет выводиться консоль "Четное число", "Нечетное число".
+// 6. Вам дана переменная s. Составьте условие, по которому в консоль будет выводится тип этой переменной - "boolean", "number", "string".
+// 7. Вам дана переменная а. Если переменная a больше одного и меньше 9-ти, то выведите 'Верно', иначе выведите 'Неверно'.
+// 8. Вам дана переменная а. Если переменная a равна трем или равна семи, то прибавьте к ней 7, иначе поделите ее на 10. Выведите новое значение переменной в консоль.
+// 9. Вам даны переменные а , b. Если переменная a равна или меньше 0, а переменная b больше или равна 5, то выведите сумму этих переменных, иначе выведите их разность (результат вычитания).
+// 10. Вам даны переменные a , b. Если переменная a больше 4-х и меньше 10-ти, или переменная b больше или равна 7-ми и меньше 17-ти, то выведите 'Верно', в противном случае выведите 'Неверно'.
+// 11. Вам дана переменная month. В ней лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+// 12. Вам дана переменная day. В ней лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+// 13. Вам дана переменная a='12345'. Проверьте, что первым символом этой переменной является цифра 1, 2 или 3. Если это так – выведите ‘да’, в противном случае выведите ‘нет’.
+// 14. Вам дана переменная age=*любое двузначное число*. Составьте условие, по которому, в консоль будет выводиться "Мне ** год", "Мне ** лет", в зависимости от возраста. Наример мне 21 год, мне 20 лет.
+// 15. Вам дана переменная time= *любое чило от 1 до 59*. Составьте условие, по которому время будет делиться на четверти. Пример: time = 12 - Первая четверть. Если число больше 60, то в консоли будет выдаваться "Неверное число".
+
+
+// let a = prompt ("ведито число")
+// console.log (a)
+// if ( a === 10 ){
+//   console.log("верно")
+// }
+// else{
+//   console.log("не верно")
+// }
+
+
+// let a = 50
+// let b = 100
+// if (a<b){
+//   console.log("a<b")
+// }
+// else{
+//   console.log("a>b")
+// }
+
+
+// let a = -2
+// if (a>0){
+//   console.log("positive")
+// }
+// else if (a===0){
+//   console.log("equal")
+// } else if (a<0){
+//   console.log ("negative")
+// }
+
+
+// let num = 45
+// if ( num % 2 === 0 ){
+//   console.log("четное")
+// }
+// else{
+//   console.log ("Нечетное")
+// }
+
+// 6 задача
+// let s = 1
+// if(typeof s === "number"){
+//   console.log (" number ")
+// }
+// else if ( typeof s === "boolean"){
+//   console.log("boolean")
+// }
+// else {
+//   console.log("string")
+// }
+
+//14 задача
+
+// let age = 3
+// if ( age>=1&& age<=4 || age===21){
+//   console.log (" мне"+age+"год")
+// } else{
+// console.log ("мне"+age+"лет")
+
+// }
+
+
+// 15 задача
+
+// let time = 59
+// if (time >= 1 && time <= 15 ){
+//   console.log("первая четверть")
+// } else if ( time >= 16 && time <= 30){
+//   console.log("вторая четверть")
+// }
+// else if (time >= 31 && time <=45){
+//   console.log("третья четверть")
+// } else if (time >= 46 && time <= 59){
+//   console.log("четвертая четверть")
+// }else{
+//   console.log(" Неверное число ")
+// }
+
+
+
+
+//let string = "Hello"
+
+//console.log(str [str.length - 1])
+
+
+// пользуется для название перемены
+//1 camelCase
+// 2kebab-case
+// 3 PascalCase
+//4 snake_case
+
+//let str = "Hello"
+//console.log(str.toLowerCase())
+
+
+//let str = "Helloo"
+
+//console.log(str.indexOf("l"))
+//console.log(str.last.indexOf("o")=== str.indexOf("o") && str.lastIndexOf("0") !==-1)
+
+
+//let str = "Hello"
+//console.log(str.slice(0,3));
+//console.log(str.substring(0,3));
+//console.log(str.substr(1.3));
+
+
+
+
+// let str = "123456"
+// console.log(+str)
+// console.log(Number(str))
+
+// let str = "123456"
+// console.log(num.toString())
+// console.log(String(num))
+// console.log(num + "")
+
+// function sumStr(a,b){
+//   return a
+// }
+
+// console.log(sumStr("4","5"))
+
+
+// let str = "Hello"
+// console.log(str.trim())
+
+// задача если маленькими буквами то не правильно 
+// let str = "c"
+
+// if (str === str.toLowerCase()){
+//   console.log(false)
+// } else{
+//   console.log(true)
+// }
+
+// let a = "mahabat"
+// console.log(a)
+
+// let a = 6
+// if (a,10){
+//   console.log( a < 10 )
+// }else{
+//   console.log(" a > 10")
+// }
+
+// let a = 10
+// if (a,10){
+//   console.log(" a < 10 ")
+// }else{
+//   console.log(" a > 10")
+// }
+
+// let a = 15
+// if (a<10){
+//   console.log(" a < 10 ")
+// }else{
+//   console.log(" a > 10")
+// }
+
+// let a = 58
+// let b = 55
+// console.log( a % b )
+
+// let num = 15
+// if (num % 2 === 0){
+//   console.log("четное")
+// }else{
+//   console.log("нечетное")
+// }
+
+
+console.log("головоломка".replaceAll("о", "а"))
